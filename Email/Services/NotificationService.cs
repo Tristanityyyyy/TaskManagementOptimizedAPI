80feb744e084b5cd9184e2b72ad16385d75319fa
Eyrown <aironegamil123@gmail.com>
2026-05-08 13:59:19 +0800

    fix: fix the error of not starting the swagger

--- Email/Services/NotificationService.cs
+++ Email/Services/NotificationService.cs
@@ -16,7 +16,7 @@ namespace TaskManagement.Services
             _context = context;
         }
 
-        public async Task NotifyAsync(int accountId, string message, int? projectId = null, int? taskId = null)
+        public Task NotifyAsync(int accountId, string message, int? projectId = null, int? taskId = null)
         {
             _context.Notifications.Add(new Notification
             {
@@ -27,6 +27,8 @@ namespace TaskManagement.Services
                 IsRead = false,
                 CreatedAt = PhTime
             });
+
+            return Task.CompletedTask;
         }
     }
 }
